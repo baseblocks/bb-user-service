545c6c6d210dfb5f3ed7513549dd57e0b0d23f49
okondratiuk <oleksii.kondratiuk@baseblocks.tech>
2020-09-27 13:28:39 +0300

    Implemented Audit trail for User using Spring Data Envers.

--- src/main/java/tech/baseblocks/dataencryption/DataEncryptionApplication.java
+++ src/main/java/tech/baseblocks/dataencryption/DataEncryptionApplication.java
@@ -2,8 +2,11 @@ package tech.baseblocks.dataencryption;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class DataEncryptionApplication {
 
 	public static void main(String[] args) {
